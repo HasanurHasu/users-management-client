3eb17068bcdf1c68b85b9dd31d3d857f8bfab68c
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-10-12 21:51:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+
+  const handleAddUser = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email};
+    console.log(user);
+
+    fetch('http://localhost:5000/users', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      const newUsers = [...users, data];
+      setUsers(newUsers);
+
+      form.reset();
+    })
+  }
+
+  return (
+    <>
+      
+      <h1>Users Management System</h1>
+
+      <p>{users.length}</p>
+
+      <form onSubmit={handleAddUser}>
+        <input type="name" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+
+      {
+        users.map(user => <p key={user.id}>{user.id} : {user.name} : {user.email} </p>)
+      }
+      
+    </>
+  )
+}
+
+export default App
